08e256d0065ab630d2473b3cba90a19dcb549d67
kmarriott <kurtis.s.marriott@gmail.com>
2017-11-06 20:20:05 -0700

    created repository and added basic to do items

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+//there are two ways to add a new task, and both are defined here, clicking the button or pressing enter
+$( function(){
+    $("#addbtn").click(function(){
+        additem();
+    });
+    $("#addinput").keyup(function(event){
+        if(event.keyCode === 13){
+            additem();
+        }
+    });
+});
+
+//this is what happens once an event happens to call the additem function
+function additem(){
+    let thelabel = $("#addinput").val();
+
+    if(thelabel !== "") {
+        let inputdiv = "<div><span>"+ thelabel + " " + "</span><button onclick='deleteitem(this)'>DELETE</button></div>";
+        $(".todocontainer").append(inputdiv);
+        $("#addinput").val("");
+    }
+}
+
+function deleteitem(element){
+    $(element).parent().remove();
+}
+
