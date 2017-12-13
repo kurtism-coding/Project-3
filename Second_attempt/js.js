526a9b7314b5aec2acdf6aa0cf21855f45662eb6
kmarriott <kurtis.s.marriott@gmail.com>
2017-12-13 09:58:00 -0700

    all functionality is met, just need to do the awesome portions

--- Second_attempt/js.js
+++ Second_attempt/js.js
@@ -1,44 +1,30 @@
 let lists = [];
 let selected = 0;
-//PROOF of CONCEPT
-/*let Number = 200;
- showNumber(Number);
-function showNumber (Number){
-    console.log(Number)
-}
-*/
 //Not sure how to use, but I want to implement:
 /*let JSON = JSON.stringify(lists);
 localStorage.setItem("lists", JSON);
 */
 
-// PROOF of CONCEPT
-/*
-whatIf("Kurtis");
-function whatIf(x){
-    hi(x);
-}
-function hi(name){
-    console.log("Hi " + name);
-}
-*/
-
+//works!
 function newList(listName){
     //console.log("newList is being called");
     let i = 0;
     for(i = 0; i < lists.length; i++) {
         lists[i].selected = false;
     }
-    let newList = {
-        name: listName,
-        tasks: [],
-        selected: true
-    };
-    selected = i;
+    if (listName !== "") {
+        let newList = {
+            name: listName,
+            tasks: [],
+            selected: true
+        };
+        selected = i;
 
-    lists.push(newList);
-    displayLists();
-    displayTasks(selected);
+        lists.push(newList);
+        displayLists();
+        displayTasks(selected);
+        //}
+    }
 }
 
 function displayLists(){
@@ -46,13 +32,15 @@ function displayLists(){
     let listDisplay = "";
     let i = 0;
     for (i = 0; i < lists.length; i++) {
-        listDisplay += "<div class='list'>" + "<p>" + "<button class='noBorder' onclick='selectList(" + i + ")'>" + lists[i].name + "</button>"
-            + "<button onclick=deleteList()>Delete</button>" + "</p>" + "<hr>";
+        listDisplay += "<div class='list'>" + "<p>" + "<button onclick=deleteList(" + i + ")>Delete</button>" +
+            "<button class='noBorder' onclick='selectList(" + i + ")'>" + lists[i].name + "</button>" + "</p>" + "<hr>";
     }
     //multiple divs with same id?^^
     document.getElementById("listContainer").innerHTML = listDisplay;
 
 }
+
+//works!
 $(function listButton(){
     //console.log("listButton called");
     $("#addList").click(function list(){
@@ -67,45 +55,8 @@ $(function listButton(){
 
     });
 });
-function newTask(selected){
-    //console.log("newTask is being called");
-    let taskInput = $("#taskInput").val();
-    let newTask = {
-        task: taskInput,
-        completed: false
 
-    };
-    lists[selected].tasks.push(newTask);
-    displayTasks(selected);
-
-}
-function displayTasks(listIndex){
-    //console.log("displayTasks called");
-    let taskDisplay = "";
-    let i = 0;
-    for (i = 0; i < lists[listIndex].tasks.length; i++){
-        taskDisplay += "<div>" + "<p>" + "<button onclick=markTask()>" + lists[listIndex].tasks[i].task + "</button>" +
-            "<button onclick=deleteTask()>Delete</button>" + "</p>";
-    }
-    document.getElementById("listContent").innerHTML = taskDisplay + "<div class='taskButtonDiv'>" + "<input type='text' id='taskInput'>" +
-        "<button id='taskButton'" + " onclick= newTask(" + selected + ")" + ">New Task</button>" + "</div>";
-        /**/
-
-}
-$(function taskButton(){
-    //console.log("taskButton called");
-    $("#taskButton").click(function(){
-        console.log("taskButton click works");
-        //newTask(selected);
-    });
-    $("#taskInput").keyup(function(event){
-        if(event.keyCode === 13){
-            console.log("taskButton enter works");
-            //newTask(selected);
-        }
-    });
-});
-
+//works!
 function selectList(listIndex){
     for(i = 0; i < lists.length; i++) {
         lists[i].selected = false;
@@ -118,26 +69,88 @@ function selectList(listIndex){
     displayTasks(listIndex);
 }
 
+//works!
 function deleteList(listIndex){
-    console.log("deleteList called");
-    delete lists[listIndex];
+    lists.splice(listIndex, 1);
+    displayLists();
+    selected = 0;
+    displayTasks(0);
 }
 
-function deleteTask(listIndex,taskIndex){
+function newTaskOnKeyUp(event) {
+    if(event.keyCode === 13){
+        newTask(selected);
+    }
+}
+
+//works!
+function newTask(selected){
+    //console.log("newTask is being called");
+    let taskInput = $("#taskInput").val();
+    if (taskInput !== "") {
+        let newTask = {
+            task: taskInput,
+            completed: false
+
+        };
+        lists[selected].tasks.push(newTask);
+        displayTasks(selected);
+    }
+}
+
+//works!
+function displayTasks(listIndex){
+    //console.log("displayTasks called");
+    let taskDisplay = "";
+    let i = 0;
+    for (i = 0; i < lists[listIndex].tasks.length; i++){
+        taskDisplay += "<div>" + "<p>" + "<button onclick=markTask(" + selected + "," + i + ")></button>" + lists[listIndex].tasks[i].task +
+            "<button class='btn btn-danger' onclick=deleteTask(" + selected + "," + i + ")>Delete</button>" + "</p>";
+    }
+    document.getElementById("tasks").innerHTML = taskDisplay + "<div class='taskButtonDiv'>" + "<input type='text' id='taskInput' onkeyup='newTaskOnKeyUp(event)'" + ">" +
+        "<button id='taskButton'" + " onclick= newTask(" + selected + ")" + ">New Task</button>" + "</div>" +
+        "<button onclick='clearMarked(selected)'>Clear Marked Tasks</button>";
+
+
+}
+
+//what is even happening with this
+// $(function taskButton(){
+//     $("body").keyup(function(event){
+//         if(event.keyCode === 13){
+//             console.log("taskButton enter works");
+//             //newTask(selected);
+//         }
+//     });
+// });
+
+//seems to work!
+function deleteTask(selected,taskIndex){
     console.log("deleteTask called");
-    delete lists[listIndex].tasks[taskIndex];
+    lists[selected].tasks.splice(taskIndex,1);
+    displayTasks(selected);
 }
 
-function clearMarked(){
+//works!
+function clearMarked(selected){
+    let t = 0;
     console.log("clearMarked called");
-    //loop through tasks array and delete any that have property: completed = true
+    for (t=0; t < lists[selected].tasks.length; t++){
+        if (lists[selected].tasks[t].completed){
+            lists[selected].tasks.splice(t,1);
+            t--;
+        }
+    }
+    displayTasks(selected);
 }
 
-function markTask(listIndex,taskIndex){
+//works!
+function markTask(selected,taskIndex){
     console.log("markTask called");
-    lists[listIndex].tasks[taskIndex].completed = true;
+    lists[selected].tasks[taskIndex].completed = true;
 }
 
+//works!
 function showLists(){
         let x = document.getElementById("listSection");
         if (x.style.display === "none") {
